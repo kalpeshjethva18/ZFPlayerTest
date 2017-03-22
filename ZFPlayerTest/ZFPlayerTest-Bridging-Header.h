64efc4ccae3ae277fbc394c812b782cd3e71ec50
fengxiaohui <18367107969@163.com>
2017-03-22 11:31:48 +0800

    change bridge header file

---
+++ ZFPlayerTest/ZFPlayerTest-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  ZFPlayerTest-Bridging-Header.h
+//  ZFPlayerTest
+//
+//  Created by xiaohui on 2017/3/22.
+//  Copyright © 2017年 xiaohui. All rights reserved.
+//
+
+//#ifndef ZFPlayerTest_Bridging_Header_h
+//#define ZFPlayerTest_Bridging_Header_h
+//
+//
+//#endif /* ZFPlayerTest_Bridging_Header_h */
+
+#import "ZFPlayer.h"
+#import "UIImageView+WebCache.h"
+#import "ZFVideoModel.h"
